9e2518f6606716e38e1653a975ff9b8ac40d47a0
Robin Ricard <ricard.robin@gmail.com>
2014-10-13 11:02:03 +0200

    Change pawn

--- engine/play.pl
+++ engine/play.pl
@@ -16,11 +16,26 @@ play_pawn(Row, Col) :-
   retract(current_player(Type)),
   assert(current_player(TypeRes)).
 
+/**
+ * Relation: change_pawn/4
+ * @1: Board - the original board
+ * @2: Row - the row to change
+ * @3: Column - the column to change
+ * @4: Type - the type to put
+ * @5: BoardRes - the new board
+ */
 change_pawn([H1|T], 0, Col, Type, [H2|T]) :- change_in_list(Col, Type, H1, H2).
 change_pawn([H|T1], Row1, Col, Type, [H|T2]) :-
   Row2 is Row1 - 1,
   change_pawn(T1, Row2, Col, Type, T2).
 
+/**
+ * Relation: change_in_list/4
+ * @1: Index - The index to change
+ * @2: Type - to this type
+ * @3: Origin - the original list
+ * @4: New - the new list
+ */
 change_in_list(0, Type, [_|T], [Type|T]).
 change_in_list(X1, Type, [H|T1], [H|T2]) :-
   X2 is X1 - 1,
