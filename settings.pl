620be9b73ec8ae8c3b4e694678c55d0e11c2055a
Robin Ricard <ricard.robin@gmail.com>
2014-10-13 21:42:31 +0200

    Working Game loop !

--- settings.pl
+++ settings.pl
@@ -1,3 +1,8 @@
 /* Module Imports */
+?- [ai/random].
+?- [ui/display].
+
 /* Reversi settings */
 display(Board) :- console_display(Board).
+ai_w(Player, Board, Row, Col) :- random_ai(Player, Board, Row, Col).
+ai_b(Player, Board, Row, Col) :- random_ai(Player, Board, Row, Col).
