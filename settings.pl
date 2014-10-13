87781a5960ab63a22b629faf1db98ba9ba7d0ba7
Robin Ricard <ricard.robin@gmail.com>
2014-10-13 22:04:12 +0200

    Add alternatives

--- settings.pl
+++ settings.pl
@@ -1,8 +1,17 @@
 /* Module Imports */
 ?- [ai/random].
+?- [ai/minmax].
+
 ?- [ui/display].
 
 /* Reversi settings */
+% Choose your display
 display(Board) :- console_display(Board).
+
+%% Choose your White Player AI
 ai_w(Player, Board, Row, Col) :- random_ai(Player, Board, Row, Col).
+% ai_w(Player, Board, Row, Col) :- minmax_ai(Player, Board, Row, Col).
+
+%% Choose your Black Player AI
 ai_b(Player, Board, Row, Col) :- random_ai(Player, Board, Row, Col).
+% ai_b(Player, Board, Row, Col) :- minmax_ai(Player, Board, Row, Col).
