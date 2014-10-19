fdabf87bd5fe09096fd443d311aa631f2d2e0118
Edouard Oger <edouard.oger@gmail.com>
2014-10-19 13:32:49 -0700

    Updated doc

--- ai/minmax.pl
+++ ai/minmax.pl
@@ -13,11 +13,12 @@ minmax_ai(Player, Board, Row, Col) :-
   minimax(Player, Board, Player, [Row, Col], _, 2).
 
 /**
-* terminal/3
-* Succeeds when the game is over and returns the score of the IA's player.
-* @1: +State - The state of the board
-* @2: +Player - The currrent player
+* terminal/4
+* Succeeds when the game is over/we are too deep in the search tree and returns the score of the IA's player.
+* @1: +Board - The state of the board
+* @2: +FavPlayer - The player the IA is playing
 * @3: -Value - The value associated to this game.
+* @4: +Depth - The current depth in the search tree
 */
 terminal(Board, FavPlayer, Value, _) :-
   is_finished(Board),
@@ -30,12 +31,14 @@ terminal(Board, FavPlayer, Value, Depth) :-
   !.
 
 /**
-* minimax/4
+* minimax/6
 * Choose the best move for a player using the minmax algorithm.
-* @1: +State - The state of the board
-* @2: +Player - The current player
-* @3: -BestMove - The best move to make
-* @4: -Value - The value (fitness) associated with the best move
+* @1: +FavPlayer - The player the IA is playing
+* @2: +Board - The state of the board
+* @3: +Player - The current player
+* @4: -BestMove - The best move to make
+* @5: -Value - The value (fitness) associated with the best move
+* @6: +Depth - The current depth in the search tree
 */
 
 minimax(FavPlayer, Board, _, _, Value, Depth) :-
@@ -47,17 +50,19 @@ minimax(FavPlayer, Board, Player, BestMove, Value, Depth) :-
   bestMove(FavPlayer, Board, Player, Moves, BestMove, Value, Depth).
   
 /**
-* bestMove/5
+* bestMove/7
 * Choose the best move between all possible move for a player.
-* @1: +Player - The player who will play this move
-* @2: +State - The board to consider
-* @3: +Moves - The possible moves for player
-* @4: -BestMove - The best move to make
-* @5: -BestValue - The value (fitness) associated to the best move to make
+* @1: +FavPlayer - The player the IA is playing
+* @2: +Board - The state of the board
+* @3: +Player - The player who will play this move
+* @4: +Moves - The possible moves for player
+* @5: -BestMove - The best move to make
+* @6: -BestValue - The value (fitness) associated to the best move to make
+* @7: +Depth - The current depth in the search tree
 */
   
 % No possible move
-bestMove(FavPlayer, Board, Player, [], [], Value, Depth) :- 
+bestMove(FavPlayer, Board, Player, [], _, Value, Depth) :- 
   !,
   NewDepth is Depth - 1,
   
@@ -88,14 +93,16 @@ bestMove(FavPlayer, Board, Player, [[Row, Col]|OtherMoves], BestMove, BestValue,
   choose(FavPlayer, Player, [Row, Col], ValueFromFirst, MoveFromTail, ValueFromTail, BestMove, BestValue).
   
 /**
-* choose/6
+* choose/8
 * Choose the best move between two moves.
-* @1: +Move1 - First move to compare
-* @2: +Val1 - The value (fitness) associated to Move1
-* @3: +Move2 - Second move to compare
-* @4: +Val2 - The value (fitness) associated to Move2
-* @5: -BestMove - The best move between both moves
-* @6: -BestValue - The value associated with the best move
+* @1: +FavPlayer - The player the IA is playing
+* @2: +Player - The player who will play this move
+* @3: +Move1 - First move to compare
+* @4: +Val1 - The value (fitness) associated to Move1
+* @5: +Move2 - Second move to compare
+* @6: +Val2 - The value (fitness) associated to Move2
+* @7: -BestMove - The best move between both moves
+* @8: -BestValue - The value associated with the best move
 */
 choose(FavPlayer, Player, Move1, Val1, _, Val2, Move1, Val1) :-
   FavPlayer == Player,
