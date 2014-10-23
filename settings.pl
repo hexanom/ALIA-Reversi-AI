4768f381c2dd4f9c4abea30585b9e993e52447b4
Edouard Oger <edouard.oger@gmail.com>
2014-10-23 05:07:04 -0700

    last warnings removed

--- settings.pl
+++ settings.pl
@@ -11,12 +11,13 @@
 
 /* Reversi settings */
 % Choose your display
-display(Board).
+display(_).
 % display(Board) :- gui_display(Board).
 
 %% Choose your White Player AI
-ai_w(Player, Board, Row, Col) :- random_ai(Player, Board, Row, Col).
+% ai_w(Player, Board, Row, Col) :- random_ai(Player, Board, Row, Col).
 % ai_w(Player, Board, Row, Col) :- minmax_ai(Player, Board, Row, Col, heuristic_positional_score).
+ai_w(Player, Board, Row, Col) :- alphabeta_ai(Player, Board, Row, Col, heuristic_score).
 
 %% Choose your Black Player AI
 % ai_b(Player, Board, Row, Col) :- random_ai(Player, Board, Row, Col).
